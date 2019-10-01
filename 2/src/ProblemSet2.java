beb8c87c5883bf1d90d1dd7c8123318401afe0a6
Harshita Garg <26150304+mercuryhg31@users.noreply.github.com>
2019-10-01 18:28:33 -0400

    Finished with at least the basics for all, working on finalizing

--- 2/src/ProblemSet2.java
+++ 2/src/ProblemSet2.java
@@ -29,27 +29,27 @@ public class ProblemSet2 {
          * Prompt the user to enter the following information (in order): first name, last
          * name, grade, age, and hometown.
          */
-        // System.out.print("\nEnter your first name: "); // first name // TODO uncomment ex 1
-        // String firstName = in.nextLine();
+        System.out.print("\nEnter your first name: "); // first name
+        String firstName = in.nextLine();
 
-        // System.out.print("Enter your last name: "); // last name
-        // String lastName = in.nextLine();
+        System.out.print("Enter your last name: "); // last name
+        String lastName = in.nextLine();
 
-        // System.out.print("Enter your grade: "); // grade
-        // int grade = in.nextInt();
-        // in.nextLine();
+        System.out.print("Enter your grade: "); // grade
+        int grade = in.nextInt();
+        in.nextLine();
 
-        // System.out.print("Enter your age: "); // age
-        // int age = in.nextInt();
-        // in.nextLine();
+        System.out.print("Enter your age: "); // age
+        int age = in.nextInt();
+        in.nextLine();
 
-        // System.out.print("Enter your hometown: "); // hometown
-        // String hometown = in.nextLine();
+        System.out.print("Enter your hometown: "); // hometown
+        String hometown = in.nextLine();
 
-        // System.out.println("\nNAME\t : " + firstName + " " + lastName);
-        // System.out.println("GRADE\t : " + grade);
-        // System.out.println("AGE\t : " + age);
-        // System.out.println("HOMETOWN : " + hometown);
+        System.out.println("\nNAME\t : " + firstName + " " + lastName);
+        System.out.println("GRADE\t : " + grade);
+        System.out.println("AGE\t : " + age);
+        System.out.println("HOMETOWN : " + hometown);
 
         /*
          * Exercise 2.
@@ -57,30 +57,30 @@ public class ProblemSet2 {
          * Given a dollar amount in the range [0.00, 1.00], print the number of dollar
          * bills, quarters, dimes, nickels, and pennies needed to produce this amount.
          */
-        // final double dollar = 1.00; // TODO uncomment ex 2 (I GOT THIS CODE TO WORK THE FIRST TIME I TESTED IT, HOLY HELL, WHAT THE HECKKKKK??? I KNOW THIS IS A BAD COMMENT BUT SORRY MR. WILSON, AHHHHHH, I'LL DELETE IT WHEN I SUBMIT, !!!)
-        // final double quarter = 0.25;
-        // final double dime = 0.10;
-        // final double nickel = 0.05;
+        final double dollar = 1.00;
+        final double quarter = 0.25;
+        final double dime = 0.10;
+        final double nickel = 0.05;
 
-        // System.out.print("\nEnter a dollar amount: ");
-        // double moneyInput = in.nextDouble();
-        // in.nextLine();
+        System.out.print("\nEnter a dollar amount: ");
+        double moneyInput = in.nextDouble();
+        in.nextLine();
 
-        // int dollarAmnt = (int) Math.floor(moneyInput / dollar);
-        // double leftover = moneyInput % dollar;
-        // int quarterAmnt = (int) Math.floor(leftover / quarter);
-        // leftover %= quarter;
-        // int dimeAmnt = (int) Math.floor(leftover / dime);
-        // leftover %= dime;
-        // int nickelAmnt = (int) Math.floor(leftover / nickel);
-        // leftover %= nickel;
-        // int pennyAmnt = (int) (leftover / 0.01);
+        int dollarAmnt = (int) Math.floor(moneyInput / dollar);
+        double leftover = moneyInput % dollar;
+        int quarterAmnt = (int) Math.floor(leftover / quarter);
+        leftover %= quarter;
+        int dimeAmnt = (int) Math.floor(leftover / dime);
+        leftover %= dime;
+        int nickelAmnt = (int) Math.floor(leftover / nickel);
+        leftover %= nickel;
+        int pennyAmnt = (int) (leftover / 0.01);
 
-        // System.out.println("DOLLARS\t: " + dollarAmnt);
-        // System.out.println("QUARTERS: " + quarterAmnt);
-        // System.out.println("DIMES\t: " + dimeAmnt);
-        // System.out.println("NICKELS\t: " + nickelAmnt);
-        // System.out.println("PENNIES\t: " + pennyAmnt + "\n");
+        System.out.println("DOLLARS\t: " + dollarAmnt);
+        System.out.println("QUARTERS: " + quarterAmnt);
+        System.out.println("DIMES\t: " + dimeAmnt);
+        System.out.println("NICKELS\t: " + nickelAmnt);
+        System.out.println("PENNIES\t: " + pennyAmnt + "\n");
 
         /*
          * Exercise 3.
@@ -88,38 +88,38 @@ public class ProblemSet2 {
          * Given a dollar amount in the range [0.00, 20.00], print the smallest number of
          * bills and coins needed to produce this amount.
          */
-        // final double tenDollar = 10.00; // TODO uncomment ex 3
-        // final double fiveDollar = 5.00;
+        final double tenDollar = 10.00;
+        final double fiveDollar = 5.00;
         
-        // System.out.print("\nEnter a dollar amount: ");
-        // moneyInput = in.nextDouble();
-        // in.nextLine();
+        System.out.print("\nEnter a dollar amount: ");
+        moneyInput = in.nextDouble();
+        in.nextLine();
 
-        // int tenDollarAmnt = (int) Math.floor(moneyInput / tenDollar);
-        // leftover = moneyInput % tenDollar;
+        int tenDollarAmnt = (int) Math.floor(moneyInput / tenDollar);
+        leftover = moneyInput % tenDollar;
 
-        // int fiveDollarAmnt = (int) Math.floor(leftover / fiveDollar);
-        // leftover %= fiveDollar;
+        int fiveDollarAmnt = (int) Math.floor(leftover / fiveDollar);
+        leftover %= fiveDollar;
 
-        // dollarAmnt = (int) Math.floor(leftover / dollar);
-        // leftover %= dollar;
+        dollarAmnt = (int) Math.floor(leftover / dollar);
+        leftover %= dollar;
 
-        // quarterAmnt = (int) Math.floor(leftover / quarter);
-        // leftover %= quarter;
+        quarterAmnt = (int) Math.floor(leftover / quarter);
+        leftover %= quarter;
 
-        // dimeAmnt = (int) Math.floor(leftover / dime);
-        // leftover %= dime;
+        dimeAmnt = (int) Math.floor(leftover / dime);
+        leftover %= dime;
 
-        // nickelAmnt = (int) Math.floor(leftover / nickel);
-        // leftover %= nickel;
+        nickelAmnt = (int) Math.floor(leftover / nickel);
+        leftover %= nickel;
 
-        // pennyAmnt = (int) (leftover / 0.01);
+        pennyAmnt = (int) (leftover / 0.01);
 
-        // int bills = tenDollarAmnt + fiveDollarAmnt + dollarAmnt;
-        // int coins = quarterAmnt + dimeAmnt + nickelAmnt + pennyAmnt;
+        int bills = tenDollarAmnt + fiveDollarAmnt + dollarAmnt;
+        int coins = quarterAmnt + dimeAmnt + nickelAmnt + pennyAmnt;
 
-        // System.out.println("BILLS : " + bills);
-        // System.out.println("COINS : " + coins + "\n");
+        System.out.println("BILLS : " + bills);
+        System.out.println("COINS : " + coins + "\n");
 
         /*
          * Exercise 4.
@@ -127,63 +127,62 @@ public class ProblemSet2 {
          * Given a number of inches, print the equivalent number of miles, yards, feet,
          * and inches.
          */
-        // final double inMi = 63360; // inches in a mile // TODO uncomment ex 4
-        // final double inYd = 36; // inches in a yard
-        // final double inFt = 12; // inches in a foot
+        final double inMi = 63360; // inches in a mile
+        final double inYd = 36; // inches in a yard
+        final double inFt = 12; // inches in a foot
 
-        // double leftover; // TODO delete
-
-        // System.out.print("\nEnter a number of inches: ");
-        // double inchesInput = in.nextDouble();
-        // in.nextLine();
+        System.out.print("\nEnter a number of inches: ");
+        double inchesInput = in.nextDouble();
+        in.nextLine();
         
-        // int mileAmnt = (int) Math.floor(inchesInput / inMi);
-        // leftover = inchesInput % inMi;
+        int mileAmnt = (int) Math.floor(inchesInput / inMi);
+        leftover = inchesInput % inMi;
 
-        // int yardAmnt = (int) Math.floor(leftover / inYd);
-        // leftover %= inYd;
+        int yardAmnt = (int) Math.floor(leftover / inYd);
+        leftover %= inYd;
 
-        // int footAmnt = (int) Math.floor(leftover / inFt);
-        // leftover %= inFt;
+        int footAmnt = (int) Math.floor(leftover / inFt);
+        leftover %= inFt;
 
-        // System.out.println("MILES\t : " + mileAmnt);
-        // System.out.println("YARDS\t : " + yardAmnt);
-        // System.out.println("FEET\t : " + footAmnt);
-        // System.out.println("INCHES\t : " + (int) leftover + "\n");
+        System.out.println("MILES\t : " + mileAmnt);
+        System.out.println("YARDS\t : " + yardAmnt);
+        System.out.println("FEET\t : " + footAmnt);
+        System.out.println("INCHES\t : " + (int) leftover + "\n");
+
         /*
          * Exercise 5.
          * 
          * Given a number of centimeters, print the equivalent number of kilometers,
          * meters, and centimeters.
          */
-        // final double cmKm = 100000; // centimeters in a kilometer // TODO uncomment ex 5
-        // final double cmM = 100; // centimeters in a meter
+        final double cmKm = 100000; // centimeters in a kilometer
+        final double cmM = 100; // centimeters in a meter
 
-        // System.out.print("\nEnter a number of centimeters: ");
-        // double cmInput = in.nextDouble();
-        // in.nextLine();
+        System.out.print("\nEnter a number of centimeters: ");
+        double cmInput = in.nextDouble();
+        in.nextLine();
         
-        // int kmAmnt = (int) Math.floor(cmInput / cmKm);
-        // leftover = cmInput % cmKm;
+        int kmAmnt = (int) Math.floor(cmInput / cmKm);
+        leftover = cmInput % cmKm;
 
-        // int mAmnt = (int) Math.floor(leftover / cmM);
-        // leftover %= cmM;
+        int mAmnt = (int) Math.floor(leftover / cmM);
+        leftover %= cmM;
 
-        // System.out.println("KILOMETERS  : " + kmAmnt); // TODO fix the formatting man, 'tis wack
-        // System.out.println("METERS      : " + mAmnt);
-        // System.out.println("CENTIMETERS : " + (int) leftover + "\n");
+        System.out.println("KILOMETERS  : " + kmAmnt); // TODO fix the formatting man, 'tis wack
+        System.out.println("METERS      : " + mAmnt);
+        System.out.println("CENTIMETERS : " + (int) leftover + "\n");
         
         /*
          * Exercise 6.
          * 
          * Given a diameter, print the area and circumference of the corresponding circle.
          */
-        // System.out.print("\nEnter a diameter: "); // TODO uncomment ex 6
-        // double diameter = in.nextDouble();
-        // in.nextLine();
+        System.out.print("\nEnter a diameter: ");
+        double diameter = in.nextDouble();
+        in.nextLine();
 
-        // System.out.println("AREA\t      : " + dec2.format(Math.PI * Math.pow((diameter/2), 2)));
-        // System.out.println("CIRCUMFERENCE : " + dec2.format(Math.PI * diameter) + "\n");
+        System.out.println("AREA\t      : " + dec2.format(Math.PI * Math.pow((diameter/2), 2)));
+        System.out.println("CIRCUMFERENCE : " + dec2.format(Math.PI * diameter) + "\n");
 
         /*
          * Exercise 7.
@@ -191,48 +190,54 @@ public class ProblemSet2 {
          * Given a length and width, print the area, perimeter, and diagonal of the
          * corresponding rectangle.
          */
-        // System.out.print("\nEnter a length: "); // TODO uncomment ex 7
-        // double rectLength = in.nextDouble();
-        // in.nextLine();
-        // System.out.print("Enter a width: ");
-        // double rectWidth = in.nextDouble();
-        // in.nextLine();
+        System.out.print("\nEnter a length: ");
+        double rectLength = in.nextDouble();
+        in.nextLine();
+        System.out.print("Enter a width: ");
+        double rectWidth = in.nextDouble();
+        in.nextLine();
 
-        // System.out.println("\nAREA\t  : " + dec2.format(rectLength * rectWidth));
-        // System.out.println("PERIMETER : " + dec2.format(rectLength * 2 + rectWidth * 2));
-        // System.out.println("DIAGONAL  : " + dec2.format(Math.sqrt(Math.pow(rectLength, 2) + Math.pow(rectWidth, 2))) + "\n");
+        System.out.println("\nAREA\t  : " + dec2.format(rectLength * rectWidth));
+        System.out.println("PERIMETER : " + dec2.format(rectLength * 2 + rectWidth * 2));
+        System.out.println("DIAGONAL  : " + dec2.format(Math.sqrt(Math.pow(rectLength, 2) + Math.pow(rectWidth, 2))) + "\n");
 
         /*
          * Exercise 8.
          * 
          * Given a side length, print the area and perimeter of the corresponding hexagon.
          */
-        // System.out.print("\nEnter a side length: "); // TODO uncomment ex 8
-        // double sideLength = in.nextDouble();
-        // in.nextLine();
+        System.out.print("\nEnter a side length: ");
+        double sideLength = in.nextDouble();
+        in.nextLine();
 
-        // System.out.println("AREA\t : " + dec2.format(Math.pow(sideLength, 2) * (3 * Math.sqrt(3) / 2)));
-        // System.out.println("PERIMETER : " + dec2.format(sideLength * 6));
+        System.out.println("AREA\t : " + dec2.format(Math.pow(sideLength, 2) * (3 * Math.sqrt(3) / 2)));
+        System.out.println("PERIMETER : " + dec2.format(sideLength * 6));
         
         /*
          * Exercise 9.
          * 
          * Given a string, reverse and print the first and second halves of that string.
          */
-        // System.out.print("\nEnter a String: "); // TODO uncomment ex 9
-        // String strInput = in.nextLine();
-        // int cutLine = (int) Math.floor(strInput.length() / 2);
+        System.out.print("\nEnter a String: ");
+        String strInput = in.nextLine();
+        int cutLine = (int) Math.floor(strInput.length() / 2);
 
-        // System.out.print("\n" + strInput.substring(cutLine));
-        // System.out.print(strInput.substring(0, cutLine) + "\n");
+        System.out.print("\n" + strInput.substring(cutLine));
+        System.out.print(strInput.substring(0, cutLine) + "\n");
         
         /*
          * Exercise 10.
          * 
          * Given a first, middle, and last name, print the corresponding initials.
          */
-        
+        System.out.print("\nEnter your first name: ");
+        firstName = in.nextLine();
+        System.out.print("Enter your middle name: ");
+        String middleName = in.nextLine();
+        System.out.println("Enter your last name: "); // TODO resolve skipping line issue
+        lastName = in.nextLine();
 
+        System.out.println("\n" + firstName.charAt(0) + middleName.charAt(0) + lastName.charAt(0));
         
         in.close();
     }
