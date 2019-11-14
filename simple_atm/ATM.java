00cfbd1b9ee04721280ff0410b517fe9ed50d14c
Harshita Garg <26150304+mercuryhg31@users.noreply.github.com>
2019-11-14 15:35:41 -0500

    Simple ATM Tutorial

---
+++ simple_atm/ATM.java
@@ -0,0 +1,112 @@
+import java.util.Scanner;
+
+public class ATM {
+
+    private Scanner in;
+    private BankAccount activeAccount;
+    
+    public static final int VIEW = 1;
+    public static final int DEPOSIT = 2;
+    public static final int WITHDRAW = 3;
+    public static final int LOGOUT = 4;
+
+    public static final int INVALID = 0;
+    public static final int INSUFFICIENT = 1;
+    public static final int SUCCESS = 2;
+    
+    public ATM() {
+        in = new Scanner(System.in);
+        activeAccount = new BankAccount(1234, new User("Ryan", "Wilson"));
+    }
+    
+    public void startup() {
+        System.out.println("Welcome to the AIT ATM!\n");
+        
+        while (true) {
+            System.out.print("Account No.: ");
+            long accountNo = in.nextLong();
+            
+            System.out.print("PIN        : ");
+            int pin = in.nextInt();
+            
+            if (isValidLogin(accountNo, pin)) {
+                System.out.println("\nHello, again, " + activeAccount.getAccountHolder().getFirstName() + "!\n");
+                
+                boolean validLogin = true;
+                while (validLogin) {
+                    switch (getSelection()) {
+                        case VIEW: showBalance(); break;
+                        case DEPOSIT: deposit(); break;
+                        case WITHDRAW: withdraw(); break;
+                        case LOGOUT: validLogin = false; break;
+                        default: System.out.println("\nInvalid selection.\n"); break;
+                    }
+                }
+            } else {
+                if (accountNo == -1 && pin == -1) {
+                    shutdown();
+                } else {
+                    System.out.println("\nInvalid account number and/or PIN.\n");
+                }
+            }
+        }
+    }
+    
+    public boolean isValidLogin(long accountNo, int pin) {
+        return accountNo == activeAccount.getAccountNo() && pin == activeAccount.getPin();
+    }
+    
+    public int getSelection() {
+        System.out.println("[1] View balance");
+        System.out.println("[2] Deposit money");
+        System.out.println("[3] Withdraw money");
+        System.out.println("[4] Logout");
+        
+        return in.nextInt();
+    }
+    
+    public void showBalance() {
+        System.out.println("\nCurrent balance: " + activeAccount.getBalance());
+    }
+    
+    public void deposit() {
+        System.out.print("\nEnter amount: ");
+        double amount = in.nextDouble();
+        
+        int status = activeAccount.deposit(amount);
+        if (status == ATM.INVALID) {
+            System.out.println("\nDeposit rejected. Amount must be greater than $0.00.\n");
+        } else if (status == ATM.SUCCESS) {
+            System.out.println("\nDeposit accepted.\n");
+        }
+    }
+    
+    public void withdraw() {
+        System.out.print("\nEnter amount: ");
+        double amount = in.nextDouble();
+            
+        int status = activeAccount.withdraw(amount);
+        if (status == ATM.INVALID) {
+            System.out.println("\nWithdrawal rejected. Amount must be greater than $0.00.\n");
+        } else if (status == ATM.INSUFFICIENT) {
+            System.out.println("\nWithdrawal rejected. Insufficient funds.\n");
+        } else if (status == ATM.SUCCESS) {
+            System.out.println("\nWithdrawal accepted.\n");
+        }
+    }
+    
+    public void shutdown() {
+        if (in != null) {
+            in.close();
+        }
+        
+        System.out.println("\nGoodbye!");
+        System.exit(0);
+    }
+    
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        
+        atm.startup();
+    }
+}
---
+++ simple_atm/BankAccount.java
@@ -0,0 +1,55 @@
+import java.text.NumberFormat;
+
+public class BankAccount {
+
+    private static long prevAccountNo = 100000000L;
+    
+    private int pin;
+    private long accountNo;
+    private double balance;
+    private User accountHolder;
+    
+    public BankAccount(int pin, User accountHolder) {
+        this.pin = pin;
+        this.accountNo = ++BankAccount.prevAccountNo;
+        this.balance = 0;
+        this.accountHolder = accountHolder;
+    }
+    
+    public int getPin() {
+        return pin;
+    }
+    
+    public long getAccountNo() {
+        return accountNo;
+    }
+    
+    public String getBalance() {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        return currency.format(balance);
+    }
+    
+    public User getAccountHolder() {
+        return accountHolder;
+    }
+    
+    public int deposit(double amount) {
+        if (amount <= 0) {
+            return ATM.INVALID;
+        } else {
+            balance += amount;
+        }
+        return ATM.SUCCESS;
+    }
+    
+    public int withdraw(double amount) {
+        if (amount <= 0) {
+            return ATM.INVALID;
+        } else if (amount > balance) {
+            return ATM.INSUFFICIENT;
+        } else {
+            balance -= amount;
+        }
+        return ATM.SUCCESS;
+    }
+}
