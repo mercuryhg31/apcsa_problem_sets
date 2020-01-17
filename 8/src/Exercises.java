c9b9227ca08fb000304c8389f1dd0b581ab534b6
Harshita Garg <26150304+mercuryhg31@users.noreply.github.com>
2020-01-17 16:35:24 -0500

    yikes, gotta commit

--- 8/src/Exercises.java
+++ 8/src/Exercises.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 
-public class Exercises { // TODO make all methods not static
+public class Exercises { // TODO make all methods not static // cd ../src/; javac Exercises.java; cd ../tests/; ./grademe.sh
 
 	// public static void main (String args[]) {
 
 	// }
 
-	public boolean commonEnd(int[] a, int[] b) {
+	public boolean commonEnd(final int[] a, final int[] b) {
 		if (a == null || a.length == 0 || b == null || b.length == 0) {
 			return false;
 		}
@@ -16,9 +16,9 @@ public class Exercises { // TODO make all methods not static
 		return false;	// default return value to ensure compilation
 	}
 
-	public String[] endsMeet(String[] values, int n) {
+	public String[] endsMeet(final String[] values, final int n) {
 		if (values == null || values.length < n || n < 0) return new String[0];
-		String[] output = new String[n*2];
+		final String[] output = new String[n*2];
 		for (int i = 0; i < n; i++) {
 			output[i] = Arrays.copyOfRange(values, 0, n)[i];
 		}
@@ -29,54 +29,75 @@ public class Exercises { // TODO make all methods not static
 		return output;
 	}
 
-	public int difference(int[] numbers) {
+	public int difference(final int[] numbers) {
 		if (numbers == null || numbers.length < 1) return -1;
 		Arrays.sort(numbers);
 		return numbers[numbers.length-1] - numbers[0];
 	}
 
-	public double biggest(double[] numbers) {
+	public double biggest(final double[] numbers) { // TODO mr. wilson??
+		if (numbers == null) return -1;
 		boolean ohno = false;
-		for (double number : numbers) {
+		for (final double number : numbers) {
 			if (number < 0) ohno = true;
 		}
-		if (numbers == null || numbers.length % 2 == 0 || ohno) return -1;
+		if (numbers.length % 2 == 0 || ohno) return -1;
 
 		Arrays.sort(numbers);
+		if (numbers[0] >= numbers[numbers.length-1]) {
+			if (numbers[0] > numbers[numbers.length-1]) {
+				return numbers[0];
+			} else if (numbers[0] > numbers[numbers.length/2]) {
+				return numbers[0];
+			} else {
+				return numbers[numbers.length/2];
+			}
+		} else if (numbers[numbers.length-1] >= numbers[numbers.length/2]) {
+			if (numbers[numbers.length-1] > numbers[numbers.length/2]) {
+				return numbers[numbers.length-1];
+			}
+			return numbers[numbers.length/2];
+		}
 
+		return -1;
 	}
 
-	public String[] middle(String[] values) {
-		// write your code here
+	public String[] middle(final String[] values) { // TODO mr. wilson???
+		if (values == null) return new String[0];
+		boolean ohno = false;
+		for (final String word : values) {
+			if (word == null) ohno = true;
+		}
+		if (values.length % 2 == 0 || ohno) return new String[0];
 
-		return null;	// default return value to ensure compilation
+		return new String[] {values[(values.length/2)-1], values[values.length/2], values[(values.length/2)+1]};
 	}
 
-	public boolean increasing(int[] numbers) {
+	public boolean increasing(final int[] numbers) {
 		// write your code here
 
 		return false;	// default return value to ensure compilation
 	}
 
-	public boolean everywhere(int[] numbers, int x) {
+	public boolean everywhere(final int[] numbers, final int x) {
 		// write your code here
 
 		return false;	// default return value to ensure compilation
 	}
 
-	public boolean consecutive(int[] numbers) {
+	public boolean consecutive(final int[] numbers) {
 		// write your code here
 
 		return false;	// default return value to ensure compilation
 	}
 
-	public boolean balance(int[] numbers) {
+	public boolean balance(final int[] numbers) {
 		// write your code here
 
 		return false;	// default return value to ensure compilation
 	}
 
-	public int clumps(String[] values) {
+	public int clumps(final String[] values) {
 		// write your code here
 
 		return -1;		// default return value to ensure compilation
