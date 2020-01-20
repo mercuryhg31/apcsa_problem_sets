bd23438a9f277262e93a7fb979f63202e411b2ed
Harshita Garg <26150304+mercuryhg31@users.noreply.github.com>
2020-01-19 20:21:43 -0500

    balance

--- 8/src/Exercises.java
+++ 8/src/Exercises.java
@@ -5,20 +5,14 @@ public class Exercises { // TODO make all methods not static // cd ../src/; java
 	public static void main (String args[]) {
 		Exercises ex = new Exercises();
 
-		System.out.println(ex.increasing(new int[] {1, 2, 3, 4, 5, 6}));
-		System.out.println(ex.increasing(new int[] {1, 2}));
-		System.out.println(ex.increasing(new int[] {1, 2, 6, 2, 4}));
-		System.out.println(ex.increasing(new int[] {9, 8, 7, 6, 5, 4, 1, 3, 4}));
-		System.out.println(ex.increasing(new int[] {9, 8, 7, 6, 5, 4}));
+		System.out.println(ex.balance(new int[] {1, 1}));
+		System.out.println(ex.balance(new int[] {1, 2, 3}));
+		System.out.println(ex.balance(new int[] {1, 3, 4}));
 	}
 
 	public boolean commonEnd(int[] a, int[] b) {
-		if (a == null || a.length == 0 || b == null || b.length == 0) {
-			return false;
-		}
-
-		if (a[0] == b[0] || a[a.length-1] == b[b.length-1]) return true;
-
+		if (a == null || a.length == 0 || b == null || b.length == 0) return false;
+		if (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]) return true;
 		return false;
 	}
 
@@ -38,7 +32,7 @@ public class Exercises { // TODO make all methods not static // cd ../src/; java
 	public int difference(int[] numbers) {
 		if (numbers == null || numbers.length < 1) return -1;
 		Arrays.sort(numbers);
-		return numbers[numbers.length-1] - numbers[0];
+		return numbers[numbers.length - 1] - numbers[0];
 	}
 
 	public double biggest(double[] numbers) { // TODO mr. wilson??
@@ -50,19 +44,19 @@ public class Exercises { // TODO make all methods not static // cd ../src/; java
 		if (numbers.length % 2 == 0 || numbers.length < 3 || ohno) return -1;
 
 		Arrays.sort(numbers);
-		if (numbers[0] >= numbers[numbers.length-1]) {
-			if (numbers[0] > numbers[numbers.length-1]) {
+		if (numbers[0] >= numbers[numbers.length - 1]) {
+			if (numbers[0] > numbers[numbers.length - 1]) {
 				return numbers[0];
-			} else if (numbers[0] > numbers[numbers.length/2]) {
+			} else if (numbers[0] > numbers[numbers.length / 2]) {
 				return numbers[0];
 			} else {
-				return numbers[numbers.length/2];
+				return numbers[numbers.length / 2];
 			}
-		} else if (numbers[numbers.length-1] >= numbers[numbers.length/2]) {
-			if (numbers[numbers.length-1] > numbers[numbers.length/2]) {
-				return numbers[numbers.length-1];
+		} else if (numbers[numbers.length - 1] >= numbers[numbers.length / 2]) {
+			if (numbers[numbers.length - 1] > numbers[numbers.length / 2]) {
+				return numbers[numbers.length - 1];
 			}
-			return numbers[numbers.length/2];
+			return numbers[numbers.length / 2];
 		}
 
 		return -1;
@@ -76,46 +70,52 @@ public class Exercises { // TODO make all methods not static // cd ../src/; java
 		}
 		if (values.length % 2 == 0 || values.length < 3 || ohno) return new String[0];
 
-		// return new String[] {values[(values.length/2)-1], values[values.length/2], values[(values.length/2)+1]};
+		// return new String[] {values[(values.length / 2) - 1], values[values.length / 2], values[(values.length / 2) + 1]};
 		return new String[0]; // TODO the above is making things go weird: figure out why
 	}
 
 	public boolean increasing(int[] numbers) {
 		if (numbers == null || numbers.length < 3) return false;
-
 		for (int i = 0; i < numbers.length-2; i++) {
-			if (numbers[i] < numbers[i+1] && numbers[i+1] < numbers[i+2]) return true;
+			if (numbers[i] < numbers[i + 1] && numbers[i + 1] < numbers[i+2]) return true;
 		}
-
 		return false;
 	}
 
 	public boolean everywhere(int[] numbers, int x) {
 		if (numbers == null || numbers.length < 1) return false;
-
-		for (int i = 1; i < numbers.length-1; i++) {
-			if (!(numbers[i] == x || numbers[i-1] == x || numbers[i+1] == x)) return false;
+		for (int i = 1; i < numbers.length - 1; i++) {
+			if (!(numbers[i] == x || numbers[i - 1] == x || numbers[i + 1] == x)) return false;
 		}
-
 		return true;
 	}
 
 	public boolean consecutive(int[] numbers) {
 		if (numbers == null || numbers.length < 3) return false;
-
-		for (int i = 1; i < numbers.length-1; i++) {
-			if (numbers[i-1] % 2 == 0 && numbers[i] % 2 == 0 && numbers[i+1] % 2 == 0) return true;
+		for (int i = 1; i < numbers.length - 1; i++) {
+			if (numbers[i - 1] % 2 == 0 && numbers[i] % 2 == 0 && numbers[i + 1] % 2 == 0) return true;
 		}
 		for (int i = 1; i < numbers.length; i++) {
-			if (numbers[i-1] % 2 != 0 && numbers[i] % 2 != 0 && numbers[i+1] % 2 != 0) return true;
+			if (numbers[i - 1] % 2 != 0 && numbers[i] % 2 != 0 && numbers[i + 1] % 2 != 0) return true;
 		}
-
-		return false;	// default return value to ensure compilation
+		return false;
 	}
 
 	public boolean balance(int[] numbers) {
-		// write your code here
-
+		if (numbers == null || numbers.length < 2) return false;
+		for (int i = 1; i < numbers.length; i++) { // if (sum(Arrays.copyOfRange(numbers, 0, i)) == sum(Arrays.copyOfRange(numbers, i, numbers.length))) return true;
+			int[] one = Arrays.copyOfRange(numbers, 0, i);
+			int[] two = Arrays.copyOfRange(numbers, i, numbers.length);
+			int a = 0;
+			int b = 0;
+			for (int j = 0; j < one.length; j++) {
+				a += one[j];
+			}
+			for (int j = 0; j < two.length; j++) {
+				b += two[j];
+			}
+			if (a == b) return true;
+		}
 		return false;	// default return value to ensure compilation
 	}
 
