f578dae209bf31daac257d67457bfeb422310bc0
Harshita Garg <26150304+mercuryhg31@users.noreply.github.com>
2020-01-19 19:38:26 -0500

    just consecutive i think?

--- 8/src/Exercises.java
+++ 8/src/Exercises.java
@@ -19,7 +19,7 @@ public class Exercises { // TODO make all methods not static // cd ../src/; java
 
 		if (a[0] == b[0] || a[a.length-1] == b[b.length-1]) return true;
 
-		return false;	// default return value to ensure compilation
+		return false;
 	}
 
 	public String[] endsMeet(String[] values, int n) {
@@ -47,7 +47,7 @@ public class Exercises { // TODO make all methods not static // cd ../src/; java
 		for (double number : numbers) {
 			if (number < 0) ohno = true;
 		}
-		if (numbers.length % 2 == 0 || ohno) return -1;
+		if (numbers.length % 2 == 0 || numbers.length < 3 || ohno) return -1;
 
 		Arrays.sort(numbers);
 		if (numbers[0] >= numbers[numbers.length-1]) {
@@ -68,13 +68,13 @@ public class Exercises { // TODO make all methods not static // cd ../src/; java
 		return -1;
 	}
 
-	public String[] middle(String[] values) { // TODO mr. wilson???
+	public String[] middle(String[] values) {
 		if (values == null) return new String[0];
 		boolean ohno = false;
 		for (String word : values) {
 			if (word == null) ohno = true;
 		}
-		if (values.length % 2 == 0 || ohno) return new String[0];
+		if (values.length % 2 == 0 || values.length < 3 || ohno) return new String[0];
 
 		// return new String[] {values[(values.length/2)-1], values[values.length/2], values[(values.length/2)+1]};
 		return new String[0]; // TODO the above is making things go weird: figure out why
@@ -87,22 +87,28 @@ public class Exercises { // TODO make all methods not static // cd ../src/; java
 			if (numbers[i] < numbers[i+1] && numbers[i+1] < numbers[i+2]) return true;
 		}
 
-		return false;	// default return value to ensure compilation
+		return false;
 	}
 
 	public boolean everywhere(int[] numbers, int x) {
 		if (numbers == null || numbers.length < 1) return false;
 
-		boolean output = true;
-		for (int i = 0; i < numbers.length-1; i++) {
-			if (!(numbers[i]))
+		for (int i = 1; i < numbers.length-1; i++) {
+			if (!(numbers[i] == x || numbers[i-1] == x || numbers[i+1] == x)) return false;
 		}
 
-		return false;	// default return value to ensure compilation
+		return true;
 	}
 
 	public boolean consecutive(int[] numbers) {
-		// write your code here
+		if (numbers == null || numbers.length < 3) return false;
+
+		for (int i = 1; i < numbers.length-1; i++) {
+			if (numbers[i-1] % 2 == 0 && numbers[i] % 2 == 0 && numbers[i+1] % 2 == 0) return true;
+		}
+		for (int i = 1; i < numbers.length; i++) {
+			if (numbers[i-1] % 2 != 0 && numbers[i] % 2 != 0 && numbers[i+1] % 2 != 0) return true;
+		}
 
 		return false;	// default return value to ensure compilation
 	}
