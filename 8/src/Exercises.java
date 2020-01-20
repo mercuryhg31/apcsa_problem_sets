511fa1fc542948a19185eb2d87ae714af79f23b4
Harshita Garg <26150304+mercuryhg31@users.noreply.github.com>
2020-01-19 19:11:08 -0500

    random

--- 8/src/Exercises.java
+++ 8/src/Exercises.java
@@ -2,11 +2,17 @@ import java.util.Arrays;
 
 public class Exercises { // TODO make all methods not static // cd ../src/; javac Exercises.java; cd ../tests/; ./grademe.sh
 
-	// public static void main (String args[]) {
+	public static void main (String args[]) {
+		Exercises ex = new Exercises();
 
-	// }
+		System.out.println(ex.increasing(new int[] {1, 2, 3, 4, 5, 6}));
+		System.out.println(ex.increasing(new int[] {1, 2}));
+		System.out.println(ex.increasing(new int[] {1, 2, 6, 2, 4}));
+		System.out.println(ex.increasing(new int[] {9, 8, 7, 6, 5, 4, 1, 3, 4}));
+		System.out.println(ex.increasing(new int[] {9, 8, 7, 6, 5, 4}));
+	}
 
-	public boolean commonEnd(final int[] a, final int[] b) {
+	public boolean commonEnd(int[] a, int[] b) {
 		if (a == null || a.length == 0 || b == null || b.length == 0) {
 			return false;
 		}
@@ -16,9 +22,9 @@ public class Exercises { // TODO make all methods not static // cd ../src/; java
 		return false;	// default return value to ensure compilation
 	}
 
-	public String[] endsMeet(final String[] values, final int n) {
+	public String[] endsMeet(String[] values, int n) {
 		if (values == null || values.length < n || n < 0) return new String[0];
-		final String[] output = new String[n*2];
+		String[] output = new String[n*2];
 		for (int i = 0; i < n; i++) {
 			output[i] = Arrays.copyOfRange(values, 0, n)[i];
 		}
@@ -29,16 +35,16 @@ public class Exercises { // TODO make all methods not static // cd ../src/; java
 		return output;
 	}
 
-	public int difference(final int[] numbers) {
+	public int difference(int[] numbers) {
 		if (numbers == null || numbers.length < 1) return -1;
 		Arrays.sort(numbers);
 		return numbers[numbers.length-1] - numbers[0];
 	}
 
-	public double biggest(final double[] numbers) { // TODO mr. wilson??
+	public double biggest(double[] numbers) { // TODO mr. wilson??
 		if (numbers == null) return -1;
 		boolean ohno = false;
-		for (final double number : numbers) {
+		for (double number : numbers) {
 			if (number < 0) ohno = true;
 		}
 		if (numbers.length % 2 == 0 || ohno) return -1;
@@ -62,42 +68,52 @@ public class Exercises { // TODO make all methods not static // cd ../src/; java
 		return -1;
 	}
 
-	public String[] middle(final String[] values) { // TODO mr. wilson???
+	public String[] middle(String[] values) { // TODO mr. wilson???
 		if (values == null) return new String[0];
 		boolean ohno = false;
-		for (final String word : values) {
+		for (String word : values) {
 			if (word == null) ohno = true;
 		}
 		if (values.length % 2 == 0 || ohno) return new String[0];
 
-		return new String[] {values[(values.length/2)-1], values[values.length/2], values[(values.length/2)+1]};
+		// return new String[] {values[(values.length/2)-1], values[values.length/2], values[(values.length/2)+1]};
+		return new String[0]; // TODO the above is making things go weird: figure out why
 	}
 
-	public boolean increasing(final int[] numbers) {
+	public boolean increasing(int[] numbers) {
+		if (numbers == null || numbers.length < 3) return false;
+
+		for (int i = 0; i < numbers.length-2; i++) {
+			if (numbers[i] < numbers[i+1] && numbers[i+1] < numbers[i+2]) return true;
+		}
+
+		return false;	// default return value to ensure compilation
+	}
+
+	public boolean everywhere(int[] numbers, int x) {
+		if (numbers == null || numbers.length < 1) return false;
+
+		boolean output = true;
+		for (int i = 0; i < numbers.length-1; i++) {
+			if (!(numbers[i]))
+		}
+
+		return false;	// default return value to ensure compilation
+	}
+
+	public boolean consecutive(int[] numbers) {
 		// write your code here
 
 		return false;	// default return value to ensure compilation
 	}
 
-	public boolean everywhere(final int[] numbers, final int x) {
+	public boolean balance(int[] numbers) {
 		// write your code here
 
 		return false;	// default return value to ensure compilation
 	}
 
-	public boolean consecutive(final int[] numbers) {
-		// write your code here
-
-		return false;	// default return value to ensure compilation
-	}
-
-	public boolean balance(final int[] numbers) {
-		// write your code here
-
-		return false;	// default return value to ensure compilation
-	}
-
-	public int clumps(final String[] values) {
+	public int clumps(String[] values) {
 		// write your code here
 
 		return -1;		// default return value to ensure compilation
