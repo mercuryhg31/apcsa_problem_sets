8c4b3aa09671f0b071b6535ebc28381912761161
Harshita Garg <26150304+mercuryhg31@users.noreply.github.com>
2020-01-29 16:04:37 -0500

    middle ish

--- 9/src/Exercises.java
+++ 9/src/Exercises.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Exercises {
 
@@ -7,7 +8,7 @@ public class Exercises {
 		if (a.get(0) == b.get(0) || a.get(a.size() - 1) == b.get(b.size() - 1)) return true;
 		return false;
 	}
-	
+
 	public ArrayList<String> endsMeet(ArrayList<String> values, int n) {
 		if (values == null || values.size() < n || n < 0) return new ArrayList<String>();
 		ArrayList<String> output = new ArrayList<String>();
@@ -19,7 +20,7 @@ public class Exercises {
 		}
 		return output;
 	}
-	
+
 	public int difference(ArrayList<Integer> numbers) {
 		if (numbers == null || numbers.size() < 1) return -1;
 		int min = numbers.get(0);
@@ -30,7 +31,7 @@ public class Exercises {
 		}
 		return max - min;
 	}
-	
+
 	public double biggest(ArrayList<Double> numbers) {
 		if (numbers == null) return -1;
 		boolean ohno = false;
@@ -54,40 +55,55 @@ public class Exercises {
 		}
 		return -1;
 	}
-	
-	public ArrayList<String> middle(ArrayList<String> values) {
-		// write your code here
-		
-		return null;	// default return value to ensure compilation
+
+	public ArrayList<String> middle(ArrayList<String> values) { // TODO mr. wilson?? test case 5: passed in numbers?
+		if (values == null) return new ArrayList<String>();
+		boolean ohno = false;
+		for (String word : values) {
+			if (word == null) ohno = true;
+		}
+		if (values.size() % 2 == 0 || values.size() < 3 || ohno) return new ArrayList<String>();
+		return new ArrayList<String>(Arrays.asList(values.get((values.size() / 2) - 1), values.get(values.size() / 2), values.get((values.size() / 2) + 1)));
+
+		// ArrayList<String> output = new ArrayList<String>();
+		// System.out.println(output.add(values.get((values.size() / 2) - 1)));
+		// System.out.println(output.add(values.get(values.size() / 2)));
+		// System.out.println(output.add(values.get((values.size() / 2) + 1)));
+
+		// for (String out : output) {
+		// 	System.out.println(out);
+		// }
+
+		// return output;
 	}
 
 	public boolean increasing(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean everywhere(ArrayList<Integer> numbers, int x) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean consecutive(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public boolean balance(ArrayList<Integer> numbers) {
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
 	public int clumps(ArrayList<String> values) {
 		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
 }
