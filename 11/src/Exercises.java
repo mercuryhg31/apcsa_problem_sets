5a19b06f529f3a1545ddeedf889285e07a6066a8
Harshita Garg <26150304+mercuryhg31@users.noreply.github.com>
2020-03-11 13:49:28 -0400

    ex 1

--- 11/src/Exercises.java
+++ 11/src/Exercises.java
@@ -1,52 +1,56 @@
 import java.util.ArrayList;
 
-public class Exercises {
+public class Exercises { // TODO test all with yet to be provided grademe.sh
 
-  public int findMe(int[] list, int target) {
-    return -1;
-  }
+	public int findMe(int[] list, int target) {
+		for(int i = 0; i < list.length; i++) {
+			if (list[i] == target) {
+				return i;
+			}
+		} return -1;
+	}
 
-  public int findMe(ArrayList<String> list, String target) {
-    return -1;
-  }
+	public int findMe(ArrayList<String> list, String target) {
+		return -1;
+	}
 
-  public int findMeFaster(ArrayList<Integer> list, int target) {
-    return -1;
-  }
+	public int findMeFaster(ArrayList<Integer> list, int target) {
+		return -1;
+	}
 
-  public int findMeFaster(String[] list, String target) {
-    return -1;
-  }
+	public int findMeFaster(String[] list, String target) {
+		return -1;
+	}
 
-  public int[] bubble(int[] list, boolean ascending) {
-    return null;
-  }
+	public int[] bubble(int[] list, boolean ascending) {
+		return null;
+	}
 
-  public ArrayList<String> bubble(ArrayList<String> list, boolean ascending) {
-    return null;
-  }
+	public ArrayList<String> bubble(ArrayList<String> list, boolean ascending) {
+		return null;
+	}
 
-  public ArrayList<Integer> insertion(ArrayList<Integer> list, boolean ascending) {
-    return null;
-  }
+	public ArrayList<Integer> insertion(ArrayList<Integer> list, boolean ascending) {
+		return null;
+	}
 
-  public String[] insertion(String[] list, boolean ascending) {
-    return null;
-  }
+	public String[] insertion(String[] list, boolean ascending) {
+		return null;
+	}
 
-  public int[] selection(int[] list, boolean ascending) {
-    return null;
-  }
+	public int[] selection(int[] list, boolean ascending) {
+		return null;
+	}
 
-  public ArrayList<String> selection(ArrayList<String> list, boolean ascending) {
-    return null;
-  }
+	public ArrayList<String> selection(ArrayList<String> list, boolean ascending) {
+		return null;
+	}
 
-  public ArrayList<Integer> merge(ArrayList<Integer> list, boolean ascending) {
-    return null;
-  }
+	public ArrayList<Integer> merge(ArrayList<Integer> list, boolean ascending) {
+		return null;
+	}
 
-  public String[] merge(String[] list, boolean ascending) {
-    return null;
-  }
+	public String[] merge(String[] list, boolean ascending) {
+		return null;
+	}
 }
